9b5e0aaae76259cb1c80878750bccee74c69c235
philipp.schreiber.41 <philipp.schreiber.41@stud.tu-darmstadt.de>
2023-02-01 18:32:41 +0100

    Added checks in trade_object.py if the result is empty (No items found for this query) or if the query for the given item is invalid. If so prints a error message and returns 0 instead of crashing the program. Added a id_to_modifier.json and a parse() method in crafting.py. When creating a item now modifier names, also used in poe.trade, can now be used instead of the id of the modifier. Updated the rf helmet strategy using the beast instead of two divines.

--- profit_calc.py
+++ profit_calc.py
@@ -13,7 +13,9 @@ import strategies.crafting as crafting
 #print("Scarab Exchange:", scarab_farms.gilded_scarab_exchange.calc_profit())
 #print("Scarab Farm:", scarab_farms.gilded_sextant_farm.calc_profit())
 
+print("Crafting RF Helmet", crafting.finish_rf_helmet_strat.calc_profit())
+
 print("Crafting Diamond Flask", crafting.finish_diamond_flask_strat.calc_profit())
-print(crafting.finish_diamond_flask_strat)
+
 
 
--- trade_object.py
+++ trade_object.py
@@ -1,3 +1,5 @@
+import sys
+
 import requests
 import json
 
@@ -19,9 +21,16 @@ class TradeObject:
             api_endpoint = "http://www.pathofexile.com/api/trade/search/" + curr_league
             query_string = self.query.get_query_string()
             query_response = ratelimited_requests.post(api_endpoint, json=query_string, headers=headers)
+            if query_response.status_code == requests.codes.bad_request:
+                sys.stderr.write("Item with this data not found. Assuming value of 0. This is not correct! \n")
+                return CurrencyAmount(0, "divine")
             query_data = json.loads(query_response.content)
 
+            if len(query_data['result']) == 0:
+                sys.stderr.write("No item found for the given mods. Assuming value of 0. This is not correct! \n")
+                return CurrencyAmount(0, "divine")
             item_id = query_data['result'][self.nth_result]
+
             query_id = query_data['id']
             item_url = "https://www.pathofexile.com/api/trade/fetch/" + item_id + "?query=" + query_id
             item_response = ratelimited_requests.get(item_url, headers=headers)
